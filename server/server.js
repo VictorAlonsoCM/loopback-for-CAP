a880c755045d697cda6d8d529922476f2077eb63
Victor Alonso <vacontrerasmeza@gmail.com>
2018-09-21 13:12:12 -0500

    Stable

--- server/server.js
+++ server/server.js
@@ -23,52 +23,54 @@ boot(app, __dirname, function(err) {
   if (require.main === module) {
     // Comment this app.start line and add following lines
     // app.start();
-    //app.io = require('socket.io')(app.start());
+    // app.io = require('socket.io')(app.start());
 
-    /************************************************** */
+    /** ************************************************ */
     var io = require('socket.io').listen(app.start());
     io.sockets.on('connection', (socket)=>{
       connections.push(socket);
       console.log('Connected %s sockets connected', connections.length);
-    
-      //Disconnect
+      // Disconnect
       socket.on('disconnect', (data)=>{
-        //Deletes the disconnected user from the users array
-        users.splice(users.indexOf(socket.username), 1);
-        updateUsernames();
+        // Deletes the disconnected user from the users array
+        // users.splice(users.indexOf(socket.username), 1);
+        // updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected %s sockets connected', connections.length);
       });
-    
-      //Send Message
-      socket.on('send message', (data)=>{
+      // Send Message to Implement later
+      /* socket.on('send message', (data)=>{
         //console.log(data);
         io.sockets.emit('new message', {msg: data, user: socket.username});
         messages.push({msg: data, user: socket.username});
+      }); */
+
+      socket.on('send message', (data)=>{
+        // console.log(data);
+        io.sockets.emit('new message', {message: data.message, nickname: data.nickname});
+        // messages.push({msg: data, user: socket.username});
       });
-    
-      //New User
-      socket.on('new user', (data, callback) => {
+      // New User
+      /* socket.on('new user', (data, callback) => {
         callback(true);
         socket.username = data;
         //console.log(data);
         users.push(socket.username);
-        reloadMessages();
-        updateUsernames();
-      });
-    
-      //Update all the users each connection
-      function updateUsernames(){
+        //reloadMessages();
+        //updateUsernames();
+      }); */
+      // Update all the users each connection
+      function updateUsernames() {
         io.sockets.emit('get users', users);
-      }
-    
-      function reloadMessages(){
+      };
+
+      function reloadMessages() {
         io.sockets.emit('get messages', messages);
-        //socket.broadcast.emit('get messages', messages);
-      }
+        // socket.broadcast.emit('get messages', messages);
+      };
     });
 
-    /*************************************************** */
+    /** ************************************************* */
 
     /* require('socketio-auth')(app.io, {
       authenticate: function (socket, value, callback) {
@@ -92,7 +94,7 @@ boot(app, __dirname, function(err) {
       } //authenticate function..
     }); */
 
-    /*let sockets = new Set();
+    /* let sockets = new Set();
     app.io.on('connection', function(socket) {
       sockets.add(socket);
       console.log(`Socket ${socket.id} added`);
